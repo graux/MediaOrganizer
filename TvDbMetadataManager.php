147166c8af411eb3950206bc64e73f7008a5e6c0
César Rodríguez <kesarr@gmail.com>
2012-10-21 17:12:14 +0200

    minor fix

--- TvDbMetadataManager.php
+++ TvDbMetadataManager.php
@@ -8,26 +8,27 @@ class TvDbMetadataManager {
     private $ApiKey = null;
     private static $instance = null;
     private $mirrors = array();
-    
+
     const URL_MIRRORS = 'http://www.thetvdb.com/api/{KEY}/mirrors.xml';
     const URL_SEARCH = 'http://www.thetvdb.com/api/GetSeries.php?seriesname={NAME}';
     const URL_SERIESDATA = 'http://www.thetvdb.com/api/{KEY}/series/{ID}/en.xml';
     const URL_SERIESEPISODES = 'http://www.thetvdb.com/api/{KEY}/series/{ID}/all/en.xml';
     const URL_POSTER = 'http://thetvdb.com/banners/{POSTER}';
-    
+
     private $seriesData = array();
-    
-    private function __construct() {
+
+    private function __construct()
+    {
         $this->ApiKey = $GLOBALS['THETVDB_KEY'];
         $mirrors = $this->requestXml(str_replace('{KEY}', $this->ApiKey, self::URL_MIRRORS));
-        
+
         foreach($mirrors as $m){
             $mask = (string)$m->typemask;
             $this->mirrors[$mask] = $m->mirrorpath;
         }
-    }   
+    }
     /**
-     * 
+     *
      * @return TvDbMetadataManager
      */
     public static function getInstance(){
@@ -36,7 +37,7 @@ class TvDbMetadataManager {
         }
         return self::$instance;
     }
-    
+
     private function requestXml($url){
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_URL, $url);
@@ -45,24 +46,24 @@ class TvDbMetadataManager {
         curl_setopt($ch, CURLOPT_HTTPHEADER, array("Accept: text/xml"));
         $response = curl_exec($ch);
         curl_close($ch);
-       
+
         //error_log($response);
-            
+
         return simplexml_load_string($response);
     }
-    
+
     /**
-     * 
+     *
      * @param MediaItemSeries $mediaItem
      */
     public function fetchMediaItemData($mediaItem){
         $searchUrl = str_replace('{NAME}', urlencode($mediaItem->name), self::URL_SEARCH);
         $searchResults = $this->requestXml($searchUrl);
-        
+
         $seriesData = $searchResults->Series;
         $mediaItem->id = intval($seriesData->seriesid);
         $mediaItem->overview = (string)$seriesData->Overview;
-        
+
         if(empty($this->seriesData[$mediaItem->id])){
             $this->seriesData[$mediaItem->id] = array('Id' => $mediaItem->id,
                                         'Overview' => $mediaItem->overview,
@@ -72,7 +73,7 @@ class TvDbMetadataManager {
         $key = $mediaItem->getEpisodeKey();
         $series = $this->seriesData[$mediaItem->id];
         $episode = $series['Episodes'][$key];
-        
+
         $mediaItem->episodeOverview = $episode['overview'];
         $mediaItem->id = $episode['id'];
         $mediaItem->episodeTitle = $episode['title'];
@@ -85,12 +86,12 @@ class TvDbMetadataManager {
     public function fetchMediaItemEpisodes($mediaItem){
         $searchUrl = str_replace('{ID}', $mediaItem->id,str_replace('{KEY}', $this->ApiKey, self::URL_SERIESEPISODES));
         $searchResults = $this->requestXml($searchUrl);
-        
+
         $poster = (string)$searchResults->Series->poster;
         $this->seriesData[$mediaItem->id]['Poster'] = str_replace('{POSTER}', $poster, self::URL_POSTER);
         $this->seriesData[$mediaItem->id]['Rating'] = floatval($searchResults->Series->Rating);
         $this->seriesData[$mediaItem->id]['Runtime'] = intval($searchResults->Series->Runtime);
-        
+
         foreach($searchResults as $episode){
             if($episode->getName() == 'Episode'){
                 $epData = array(
